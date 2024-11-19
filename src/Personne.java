8eba294e2f27ece6d494c7b893618c07607b0113
Drouvot Melody <melody.drouvot@gmail.com>
2024-11-19 17:39:51 +0100

    Ajout findByID

--- src/Personne.java
+++ src/Personne.java
@@ -1,12 +1,26 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Personne {
     private int id;
     private String nom;
     private String prenom;
 
-    public Personne(String nom, String prenom) {
-        this.id = -1;
+    public Personne(int id, String nom, String prenom) {
+        this.id = id;
         this.nom = nom;
         this.prenom = prenom;
+    }
+
+    public Personne findById(int id) throws SQLException {
+        Connection connection = DBConnection.getConnection();
+        String requete = "SELECT * FROM Personne WHERE id = ?";
+        PreparedStatement prep = connection.prepareStatement(requete);
+        prep.setInt(1, 2);
+        ResultSet rs = prep.executeQuery();
+        Personne res = new Personne(rs.getInt("ID"), nom = rs.getString("NOM"), rs.getString("PRENOM"));
     }
 
     public int getId() {
